1d2126027c65ad5ffec29976c2a69d66eec10d8f
MrGoc <mrgoc@live.com>
2023-02-10 14:03:41 +0100

    Added handler for numbers, decimal point and partialy for operators.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useReducer } from "react";
 
 const buttons = [
   { id: "clear", caption: "AC", class: "button" },
@@ -21,29 +21,161 @@ const buttons = [
   { id: "decimal", caption: ".", class: "button" },
 ];
 
+const initalState = {
+  currentValue: "0",
+  history: "",
+};
+
+function handleNumbers(state, action) {
+  let newHistory = state.history;
+  let newCurentValue = state.currentValue;
+  const myValue = buttons.find(({ id }) => id === action.type);
+  if (newHistory === "") newHistory = myValue.caption;
+
+  if (
+    newCurentValue === "*" ||
+    newCurentValue === "-" ||
+    newCurentValue === "/" ||
+    newCurentValue === "x"
+  ) {
+    newCurentValue = "";
+  }
+
+  if (action.type !== "zero") {
+    if (state.currentValue === "0") {
+      newCurentValue = myValue.caption;
+      newHistory = myValue.caption;
+    } else {
+      newCurentValue = newCurentValue + myValue.caption;
+      newHistory = newHistory + myValue.caption;
+    }
+  }
+  if (action.type === "zero" && state.currentValue !== myValue.caption) {
+    newCurentValue = newCurentValue + myValue.caption;
+    newHistory = newHistory + myValue.caption;
+  }
+  return {
+    currentValue: newCurentValue,
+    history: newHistory,
+  };
+}
+
+function handleDecimalPoint(state, action) {
+  let newHistory = state.history;
+  let newCurentValue = state.currentValue;
+
+  if (state.currentValue === "0") {
+    newCurentValue = newCurentValue + ".";
+    newHistory = newCurentValue;
+  } else if (newHistory.indexOf(".") === -1) {
+    newCurentValue = newCurentValue + ".";
+    newHistory = newHistory + ".";
+  }
+
+  return {
+    currentValue: newCurentValue,
+    history: newHistory,
+  };
+}
+
+function handleOperators(state, action) {
+  let newHistory = state.history;
+  let newCurentValue = state.currentValue;
+  let last2Char = newHistory.slice(-2);
+  const myValue = buttons.find(({ id }) => id === action.type);
+
+  if (newHistory === "" && action.type === "subtract") {
+    newCurentValue = myValue.caption;
+    newHistory = myValue.caption;
+  } else if (
+    newHistory.length === 1 &&
+    newHistory !== "*" &&
+    newHistory !== "-" &&
+    newHistory !== "/" &&
+    newHistory !== "x"
+  ) {
+    newCurentValue = myValue.caption;
+    newHistory = newHistory + myValue.caption;
+  }
+  /*
+  if (
+    buttons.findIndex((el) => el.caption === last2Char[0]) > -1 &&
+    action.type !== "subtract"
+  ) {
+    newCurentValue = myValue.caption;
+    newHistory = newHistory.slice(0, newHistory.length - 3) + myValue.caption;
+  } else if (last2Char[1] === myValue.caption) {
+    newCurentValue = myValue.caption;
+    newHistory = newHistory + myValue.caption;
+  }
+*/
+  return {
+    currentValue: newCurentValue,
+    history: newHistory,
+  };
+}
+
+function reducer(state, action) {
+  let newHistory = state.history;
+  let newCurentValue = state.currentValue;
+  switch (action.type) {
+    case "clear":
+      return initalState;
+    case "one":
+    case "two":
+    case "three":
+    case "four":
+    case "five":
+    case "six":
+    case "seven":
+    case "eight":
+    case "nine":
+    case "zero":
+      return handleNumbers(state, action);
+    case "decimal":
+      return handleDecimalPoint(state, action);
+    case "divide":
+    case "multiply":
+    case "subtract":
+    case "add":
+      return handleOperators(state, action);
+    default:
+      return initalState;
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initalState);
+
   return (
     <div className="calculator-panel">
-      <Display />
-      <Calculator />
+      <Display state={state} />
+      <Calculator dispatch={dispatch} />
     </div>
   );
 }
 
-function Display() {
+function Display({ state }) {
   return (
     <div id="display">
-      <span id="result">result</span>
-      <span id="current">current</span>
+      <span id="result">{state.history}</span>
+      <span id="current">{state.currentValue}</span>
     </div>
   );
 }
 
-function Calculator() {
+function Calculator({ dispatch }) {
   return (
     <div id="calculator">
       {buttons.map((btn) => (
-        <button type="button" className={btn.class} id={btn.id}>
+        <button
+          type="button"
+          className={btn.class}
+          id={btn.id}
+          onClick={() => {
+            dispatch({ type: btn.id });
+          }}
+        >
           {btn.caption}
         </button>
       ))}
